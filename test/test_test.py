feeed5b2efd8d098d371aae9bddbfc20b61d2084
Lancelot <1984737645@qq.com>
2023-02-15 22:13:48 +0800

    :art: chore: fix lint

---
+++ test/test_test.py
@@ -0,0 +1,16 @@
+# coding: UTF-8
+
+def test_add():
+    assert 1 + 1 == 2
+
+
+def test_sub():
+    assert 1 - 1 == 0
+
+
+def test_mul():
+    assert 1 * 1 == 1
+
+
+def test_div():
+    assert 1 / 1 == 1
--- test/utils_test.py
+++ test/utils_test.py
@@ -1,7 +1,6 @@
 # coding: UTF-8
 
 
-import importlib
 from unittest import TestCase
 
 
